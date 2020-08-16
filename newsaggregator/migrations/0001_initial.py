32912bc6613d650f1901bd4507bd6f462612db59
Mohammad Fuzail <md.fuzail96@gmail.com>
2020-08-16 13:37:38 +0530

    All files done. Views added, urls added.

--- newsaggregator/migrations/0001_initial.py
+++ newsaggregator/migrations/0001_initial.py
@@ -15,8 +15,9 @@ class Migration(migrations.Migration):
             name='Article',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=200)),
-                ('url', models.TextField()),
+                ('title', models.CharField(max_length=200, unique=True)),
+                ('url', models.TextField(unique=True)),
+                ('source', models.CharField(max_length=200)),
             ],
         ),
     ]
--- newsaggregator/views.py
+++ newsaggregator/views.py
@@ -1,66 +1,55 @@
-from django.shortcuts import render, redirect
 import requests
 from bs4 import BeautifulSoup
+from django.shortcuts import render, redirect
+
 from newsaggregator.models import Article
 
 
 def scrape(request):
+    a1 = Article.objects.all()
+    a1.delete()
     session = requests.session()
-    session.headers = {'User-Agent':'Googlebot/2.1 (+http://www.google.com/bot.html)'}
-    url1 = "https://www.forbes.com/real-estate/#75f106bd730e"
+    session.headers = {'User-Agent': 'Googlebot/2.1 (+http://www.google.com/bot.html)'}
+    url1 = "https://www.zillow.com/blog/"
+
+    content1 = session.get(url1, verify=False).content
+    soup = BeautifulSoup(content1, "html.parser")
+
+    for article in soup.find_all('article', {"class": "post story"}):
+        main1 = article.find_all('div', {"class": "col-sm-8"})[0]
+        main2 = main1.find_all('h5', {"class": "post-title"})[0]
+        main = main2.find_all('a')[0]
+        title = main.text.strip()
+        link = main["href"]
+        new_article = Article()
+        new_article.source = "Zillow"
+        new_article.title = title
+        new_article.url = link
+        new_article.save()
+
+
     url2 = "https://realtytimes.com/"
-    u= [url1, url2]
+    content2 = session.get(url2, verify=False).content
+    soup2 = BeautifulSoup(content2, "html.parser")
 
-    for url in u:
-        if url==url1:
-            content = session.get(url, verify = False).content
-            soup = BeautifulSoup(content, "html.parser")
-
-
-            for article in soup.find_all('article', {
-                "class": "stream-item et-promoblock-removeable-item et-promoblock-star-item"}):
-                main = article.find_all('a')[0]
-                title = main.text.strip()
-                link = main['href']
-                new_article = Article()
-                new_article.source = "Forbes"
-                new_article.title = title
-                new_article.url = link
-                new_article.save()
-
-        elif url==url2:
-            content = session.get(url, verify=False).content
-            soup = BeautifulSoup(content, "html.parser")
-
-            for h3 in soup.find_all('h3', {"class":"sppb-article-title"}):
-                main=h3.find_all('a')[0]
-                title=main.text.strip()
-                link=main['href']
-                new_article = Article()
-                new_article.source = "RealtyTimes"
-                new_article.title = title
-                new_article.url = link
-                new_article.save()
+    for h3 in soup2.find_all('h3', {"class": "sppb-article-title"}):
+        main = h3.find_all('a')[0]
+        title = main.text.strip()
+        link = "https://realtytimes.com" + main["href"]
+        new_article = Article()
+        new_article.source = "RealtyTimes"
+        new_article.title = title
+        new_article.url = link
+        new_article.save()
 
     return redirect("/")
 
 
 def news_list(request):
-    articles1 = Article.objects.filter(source = "Forbes")[::-1]
-    articles2 = Article.objects.filter(source = "RealtyTimes")[::-1]
+    articles1 = Article.objects.filter(source="Zillow")[::-1]
+    articles2 = Article.objects.filter(source="RealtyTimes")[::-1]
     context = {
-        'object_list': articles1,
-        'objects': articles2
+        'object1': articles1,
+        'object2': articles2
     }
     return render(request, "home.html", context)
-
-
-
-
-
-
-
-
-
-
-
